665d9d9a809c61d3516e12f58f8a3b7ac1cb2d02
hasankarim18 <hallysust@gmail.com>
2024-01-14 22:55:47 +0600

    higher order component

---
+++ src/utils/higherOrder.js
@@ -0,0 +1,13 @@
+const wrapperFunction = (func) => {
+  return (num) => {
+    return func(num) * 2;
+  };
+};
+
+const addOne = (num) => {
+  return num + 1;
+};
+
+const addOneAndDouble = wrapperFunction(addOne);
+
+console.log(addOneAndDouble(4));
